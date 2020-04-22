7b264e56b1d4e75344b6ba3b270f97c9a0153f02
DeVivo <JDeVivo@nps.gov>
2020-04-22 13:07:18 -0600

    initial commit

---
+++ R/geographicFuzzing.R
@@ -0,0 +1,81 @@
+#' Convert coordinates for a location into a polygon to obscure specific location
+#'
+#' \code{dp_fuzzLocation} "fuzzes" a specific location to something less prciese prior to public release of information
+#' about sensitive resources for which data are not to be released to the public. This function takes coordinates in either UTM
+#' or decimal degrees, converts to UTM (if in decimal degrees), creates a bounding box based on rounding of UTM coordinates to
+#' the 100s or 1000s place, and then creating a polygon from the resultant points. The function returns a string in Well-Known-Text
+#' format.
+#'
+#' @lat latitude in either UTMs or decimal degrees.
+#' @lon longitude in either UTMs or decimal degrees
+#' @coordrefsys coordinate reference system. Either 4326 for decimal degrees / WGS84 datum, or 326xx for UTMs / WGS84 datum, where the xx
+#' is the northern UTM zone. For example 32616 is for UTM zone 16N.
+#' @fuzzlevel either 100m or 1000m.
+#'
+#' @examples
+#' dp_fuzzLocation(703977,4035059,32616,"Fuzzed - 1km")
+#' dp_fuzzLocation(36.43909,-84.72429,4326,"Fuzzed - 1km")
+
+dp_fuzzLocation<-function(lat,lon,coordrefsys,fuzzlevel){
+  # for decimal degrees, convert to UTM locations and identify proper crs
+  if (coordrefsys==4326){
+
+    long2UTM <- function(long) {
+      ## Function to get the UTM zone for a given longitude
+      (floor((long + 180)/6) %% 60) + 1
+    }
+
+    tempcrs<-if(lat>0){long2UTM(lon)+32600} else {long2UTM(lon)+32700}
+
+    point<-st_point(c(lon,lat))
+    point<-st_sfc(point,crs=4326)
+    pointutm<-st_transform(x = point, crs = tempcrs)
+    locationlat<-pointutm[[1]][1]
+    locationlon<-pointutm[[1]][2]
+
+  } else {
+    locationlat<-lat
+    locationlon<-lon
+  }
+
+  # do rounding of UTMs based on fuzzlevel
+  if (fuzzlevel=="Fuzzed - 1km"){
+    fuzzfactor<-1000
+  } else if (fuzzlevel=="Fuzzed - 100m") {
+    fuzzfactor<-100
+  } else { fuzzfactor <-1}
+
+  locationlat<-locationlat/fuzzfactor
+  locationlon<-locationlon/fuzzfactor
+  locationlathi<-ceiling(locationlat)*fuzzfactor
+  locationlatlo<-floor(locationlat)*fuzzfactor
+  locationlonhi<-ceiling(locationlon)*fuzzfactor
+  locationlonlo<-floor(locationlon)*fuzzfactor
+
+  # construct simple feature geometry polygon from UTM points
+  polygon_list=list(rbind(c(locationlatlo,locationlonlo),
+                          c(locationlatlo,locationlonhi),
+                          c(locationlathi,locationlonhi),
+                          c(locationlathi,locationlonlo),
+                          c(locationlatlo,locationlonlo)))
+  utmsfg<-sf::st_polygon(polygon_list)
+
+  # convert sfg to sfc with appropriate crs
+  if (coordrefsys==4326){
+    utmsfc<-sf::st_sfc(utmsfg,crs=tempcrs)
+  } else {
+    utmsfc<-sf::st_sfc(utmsfg,crs=coordrefsys)
+  }
+
+  # convert polygon to decimal degrees
+  wkt<-st_transform(x=utmsfc,crs=4326)
+  wkt<-st_as_text(wkt)
+
+  # return WKT string
+  return(wkt)
+
+}
+
+
+
+
---
+++ R/geography.R
@@ -0,0 +1,49 @@
+#' Retrieve the polygon information for the park unit from NPS REST services
+#'
+#' \code{qc_getParkPolygon} retrieves a geojson string for a polygon of a park unit. This is not the official boundary.
+#'#'
+#' @param Unit_Code is the four-character unit code as designated by NPS.
+#'
+#' @examples
+#' qc_getParkPolygon("OBRI")
+qc_getParkPolygonIRMA<-function(Unit_Code){
+  # get geography from NPS Rest Services
+  UnitsURL<-paste0('https://irmaservices.nps.gov/v2/rest/unit/',Unit_Code,'/geography')
+  xml<-httr::content(httr::GET(UnitsURL))
+
+  # Create spatial feature from polygon info returned from NPS
+  parkpolygon<-sf::st_as_sfc(xml[[1]]$Geography,geoJSON=TRUE)
+
+  return(parkpolygon)
+
+}
+
+#' Check wether a coordinate pair is within the polygon of a park unit
+#'
+#' \code{qc_ValidateCoordinates} compares a coordinate pair (in decimal degrees) to the polygon for a park unit as provided through the NPS
+#' Units rest services. The function returns a value of TRUE or FALSE.
+#'
+#'
+#' @param Unit_Code is the four-character unit code as designated by NPS.
+#' @param lat latitude, in decimal degrees
+#' @param lon longitude, in decimal degrees.
+#' @param validationsource is either "IRMA" (for the polygon provided by the NPS rest services), or
+#' "OPBOUNDS" for the park boundary included in the operational boundaries data set (not currently functional).
+#'
+#' @examples
+#' qc_ValidateCoordinates("OBRI",36.07951,-84.65610,"IRMA")
+qc_ValidateCooridnates<-function(Unit_Code,lat,lon,validationsource){
+
+  # get geography from NPS Rest Services
+  park<-qc_getParkPolygonIRMA(Unit_Code)
+
+  # Create point feature from latitude and longitude
+  point<-sf::st_point(c(lon,lat),dim="XY")
+
+  # Test whether the coordinates provided are within the polygon spatial feature
+  result<-sf::st_covers(park,point,sparse=FALSE)[,1]
+
+  return(result)
+}
+
+
