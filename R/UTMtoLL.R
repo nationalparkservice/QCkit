b7c24cb38b0e5e3fea2a20444254da68c529e45f
RobLBaker-nps <robert_baker@nps.gov>
2022-09-08 14:38:36 -0600

    Add DCColcheck, UTMtoLL, and TEcheck  (#1)
    * delete loadPackage.R
    
    * delete hello.rd
    
    * update documentation of dp_fuzzlocation.R
    
    * update documentation roxygen standards
    
    * update documentation: grammar
    
    * add @export to documentation
    
    * add @export to documentation
    
    * updated rerun devtools::document()
    
    * update title, authors, description, maintainer
    
    * add orcid to authors
    
    * DCColCheck update (#1)
    
    * Initial Commit
    
    * Added DCCheck to namespace
    
    * Added to the description
    
    * User manual added
    
    * Added dates, references,  and more user-friendly detail into the manual
    
    * Space added
    
    * Manual updated with more detail and references
    
    * Changed function name to match file name
    
    * Removed unnecessary exports
    
    Co-authored-by: Quevedo <iquevedo@nps.gov>
    
    * styler for consistent code formating (spaces, line breaks, indents, etc)
    
    * New Updated Conversion Function (#2)
    
    * Initial Commit
    
    * Added DCCheck to namespace
    
    * Added to the description
    
    * User manual added
    
    * Added dates, references,  and more user-friendly detail into the manual
    
    * Space added
    
    * Manual updated with more detail and references
    
    * Changed function name to match file name
    
    * Removed unnecessary exports
    
    * Removed ConversionUTMtoLL file and added UTMtoLL file with updated function. New function now preserves missing coordinates, uploads necessary packages and mutates the original dataframe to add two new longlat columns. Namespace updated.
    
    * Functions will no longer install packages for the user. TECheck function added, namespace updated.
    
    Co-authored-by: Quevedo <iquevedo@nps.gov>
    
    * UTMtoLL and TECheck updated (#3)
    
    * Initial Commit
    
    * Added DCCheck to namespace
    
    * Added to the description
    
    * User manual added
    
    * Added dates, references,  and more user-friendly detail into the manual
    
    * Space added
    
    * Manual updated with more detail and references
    
    * Changed function name to match file name
    
    * Removed unnecessary exports
    
    * Removed ConversionUTMtoLL file and added UTMtoLL file with updated function. New function now preserves missing coordinates, uploads necessary packages and mutates the original dataframe to add two new longlat columns. Namespace updated.
    
    * Functions will no longer install packages for the user. TECheck function added, namespace updated.
    
    Co-authored-by: Quevedo <iquevedo@nps.gov>
    
    * added dependencies to Imports: sf, terra, plyr, dplyr, tibble, sf
    
    * Fix Imports formatting
    
    * update to include fair warnign on function use and solicit user input
    
    * Ran styler for pretty code
    
    Co-authored-by: iquevedo123 <108541741+iquevedo123@users.noreply.github.com>
    Co-authored-by: Quevedo <iquevedo@nps.gov>

---
+++ R/UTMtoLL.R
@@ -0,0 +1,54 @@
+#' Coordinate Conversion from UTM to Latitude and Longitude
+#'
+#' @description UTMtoLL takes your dataframe with UTM coordinates in separate Easting and Northing columns, and adds on an additional two columns with the converted decimalLatitude and decimalLongitude coordinates using the reference coordinate system WGS84.
+#'
+#' @details Define the name of your dataframe, the easting and northing columns within it, the UTM zone within which those coordinates are located, and the reference coordinate system (datum). UTM Northing and Easting columns must be in separate columns prior to running the function.
+#'
+#' If a datum is not defined, the function will default to "WGS84".
+#'
+#' If there are missing coordinates in your dataframe they will be preserved, however they will be moved to the end of your dataframe.
+#'
+#' @param Data - The dataframe with UTM coordinates you would like to convert. Input the name of your dataframe.
+#' @param EastingCol - The name of your Easting UTM column. Input the name in quotations, ie. "EastingCol".
+#' @param NorthingCol - The name of your Northing UTM column. Input the name in quotations, ie. "NorthingCol".
+#' @param Zone - The UTM Zone. Input the zone number in quotations, ie. "17".
+#' @param Datum - The datum used in the coordinate reference system of your coordinates. Input in quotations, ie. "WGS84"
+#'
+#' @return The function returns your dataframe, mutated with an additional two columns of decimal Longitude and decimal Latitude.
+#' @export
+#'
+#' @examples UTMtoLL(Data = mydataframe, EastingCol = "EastingCoords", NorthingCol = "NorthingCoords", Zone = "17", Datum = "WGS84")
+#'
+UTMtoLL <- function(Data, EastingCol, NorthingCol, Zone, Datum = "WGS84") {
+  Base <- as.data.frame(Data)
+  Base <- dplyr::rename(Base, "b" = EastingCol, "a" = NorthingCol)
+
+  Mid <- Base[!is.na(Base$"b" & Base$"a"), ]
+
+  Mid2 <- Base[is.na(Base$"b" & Base$"a"), ]
+
+  Final <- dplyr::select(Mid, "b", "a")
+
+  Final[1:2] <- lapply(Final[1:2], FUN = function(z) {
+    as.numeric(z)
+  })
+
+
+  Final <- cbind(Final$b, Final$a)
+
+
+
+  v <- terra::vect(Final, crs = paste0("+proj=utm +zone=", Zone, " +datum=", Datum, " +units=m"))
+  v
+
+  converted <- terra::project(v, "+proj=longlat +datum=WGS84")
+  converted
+
+  lonlat <- terra::geom(converted)[, c("x", "y")]
+
+
+  Data <- cbind(Mid, lonlat)
+  Data <- plyr::rbind.fill(Data, Mid2)
+  Data <- dplyr::rename(Data, EastingCol = "b", NorthingCol = "a", "decimalLongitude" = x, "decimalLatitude" = y)
+  return(Data)
+}
--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # IMDQC
-This package is a collection of quality control functions to check, flag, and correct data collected as a 
-part of the NPS Inventory & Monitoring Program. 
+This package is a collection of quality control functions to check, flag, and correct data collected by the U.S. National Park Service Inventory & Monitoring Division.
+
+Functions are typically user generated and so may not have been thoroughly tested for all use cases.
+
+If you would like to contribute useful functions please initiate a pull request or contact robert_baker@nps.gov
 
 ## Install from GitHub
 remotes::install_github("nationalparkservice/IMDQC")
