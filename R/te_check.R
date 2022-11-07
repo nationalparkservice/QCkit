e34b6eaa48b27a178e916b483bb3fd9c661c3da5
Rob Baker <robert_baker@nps.gov>
2022-11-07 15:33:10 -0700

    new .R file names match up with new function names

---
+++ R/te_check.R
@@ -0,0 +1,44 @@
+#' Threatened Or Endangered Species Checker Function
+#'
+#' @description te_check creates a subset of the species list you provide, all of which fall under the Federal Conservation List Taxa for Data Protection. These species should be removed from your dataset if your data is to be made public.
+#'
+#' @details Define your species dataset name, column name with the scientific names of your species, and your four letter park code.
+#'
+#' The function downloads the Federal Conservation list using the IRMA odata API service and matches this species list to the list of scientific names in your dataframe.
+#' 
+#' Keep in mind that this is a Federal list, not a state list. Changes in taxa names may also cause some species to be missed.
+#' 
+#' Because the Federal Conservation list is not publicly available (???), you must be logged in to the NPS VPN or in the office to use this function.
+#' 
+#'  
+#'
+#'
+#' @param df - The name of your dataframe containing species observations
+#' @param species_col - The name of the column within your dataframe containing the scientific names of the species
+#' @param park_code -  The four letter park code
+#'
+#' @return The function returns a list in your global environment called "TorE" with the names of all the species that fall under the federal conservation list.
+#' @importFrom magrittr %>%
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' te_check(df = my_species_dataframe, species_col = "scientificNames", park_code = "BICY")
+#' }
+#'
+te_check <- function(df, species_col, park_code) {
+
+  fedlist <- ODataQuery::retrieve_data(paste0("https://irmadev.nps.gov/PrototypeCSVtoAPI/odata/FederalConservationListTaxaforDataProtection2272462?$filter=ParkCode%20eq%20%27", park_code, "%27%20or%20ParkCode%20eq%20%27All%27"))
+
+  fedlist <- as.data.frame(fedlist$value$ProtectedSci)
+
+  fedlist <- plyr::rename(fedlist, c("fedlist$value$ProtectedSci" = species_col))
+
+  Species <- tibble::tibble(df) %>%
+    dplyr::select(species_col)
+
+  TorE <- dplyr::semi_join(Species, fedlist, by = species_col) %>%
+    dplyr::select("Remove Me!" = species_col)
+
+  return(TorE)
+}
