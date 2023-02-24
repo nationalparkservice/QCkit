f5a83dfc6f4288d861cea244a634d66a21cf24a5
Judd Patterson <judd_patterson@nps.gov>
2023-02-24 14:32:24 -0700

    updating description and details

--- R/geography.R
+++ R/geography.R
@@ -47,13 +47,18 @@ validate_coord <- function(unit_code, lat, lon) {
   return(result)
 }
 
+QCkit::fuzz_location()
+QCkit::te_check()
 
-#' Convert coordinates for a location into a polygon to obscure specific location
+
+#' Convert Coordinates Into a Polygon to Obscure Specific Location
 #'
-#' \code{fuzz_location} "fuzzes" a specific location to something less precise prior to public release of information
-#' about sensitive resources for which data are not to be released to the public. This function takes coordinates in either UTM or decimal degrees, converts to UTM (if in decimal degrees), creates a bounding box based on rounding of UTM coordinates to the 100s or 1000s place, and then creating a polygon from the resultant points. The function returns a string in Well-Known-Text
-#' format.
+#' @description `fuzz_location()` "fuzzes" a specific location to something less precise prior to public release of information about sensitive resources for which data are not to be released to the public. This function takes coordinates in either UTM or decimal degrees, converts to UTM (if in decimal degrees), creates a bounding box based on rounding of UTM coordinates, and then creates a polygon from the resultant points. The function returns a string in Well-Known-Text format.
 #'
+#' @details Details will be defined later.
+#'
+#' \code{fuzz_location} 
+#' 
 #' @param lat latitude in either UTMs or decimal degrees.
 #' @param lon longitude in either UTMs or decimal degrees
 #' @param coord_ref_sys coordinate reference system. Either 4326 for decimal degrees / WGS84 datum, or 326xx for UTMs / WGS84 datum, where the xx
