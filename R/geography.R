ab037eb7ef1f2d6ab2086c52763351cd01a82c3b
Judd Patterson <judd_patterson@nps.gov>
2023-02-24 14:42:38 -0700

    adjusted help parameters

--- R/geography.R
+++ R/geography.R
@@ -52,14 +52,11 @@ validate_coord <- function(unit_code, lat, lon) {
 #' @description `fuzz_location()` "fuzzes" a specific location to something less precise prior to public release of information about sensitive resources for which data are not to be released to the public. This function takes coordinates in either UTM or decimal degrees, converts to UTM (if in decimal degrees), creates a bounding box based on rounding of UTM coordinates, and then creates a polygon from the resultant points. The function returns a string in Well-Known-Text format.
 #'
 #' @details Details will be defined later.
-#'
-#' \code{fuzz_location} 
 #' 
-#' @param lat latitude in either UTMs or decimal degrees.
-#' @param lon longitude in either UTMs or decimal degrees
-#' @param coord_ref_sys coordinate reference system. Either 4326 for decimal degrees/WGS84 datum, or 326xx for UTM/WGS84 datum, where the xx
-#' is the northern UTM zone. For example 32616 is for UTM zone 16N.
-#' @param fuzz_level Use "Fuzzed - 10km", "Fuzzed - 1km", or "Fuzzed - 100m"
+#' @param lat - The latitude in either UTMs or decimal degrees.
+#' @param lon - The longitude in either UTMs or decimal degrees
+#' @param coord_ref_sys - The EPSG coordinate system of the latitude and longitude coordinates. Either 4326 for decimal degrees/WGS84 datum, or 326xx for UTM/WGS84 datum, where the xx is the northern UTM zone. For example 32616 is for UTM zone 16N.
+#' @param fuzz_level - Use "Fuzzed - 10km", "Fuzzed - 1km", or "Fuzzed - 100m"
 #'
 #' @export
 #'
