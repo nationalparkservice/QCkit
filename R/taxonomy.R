743245703dab3d098f112bc7ad74facc223176ae
Rob Baker <robert_baker@nps.gov>
2024-01-08 10:28:52 -0700

    fix deprecated function.

--- R/taxonomy.R
+++ R/taxonomy.R
@@ -296,7 +296,7 @@ te_check <- function(x, species_col, park_code, expansion = FALSE) {
   # add column explaining Fed T and E codes. From:
   # https://ecos.fws.gov/ecp0/html/db-status.html
   #---- code folding ----
-  fedspp < -fedspp %>% dplyr::mutate(status = dplyr::case_when(
+  fedspp<-fedspp %>% dplyr::mutate(status = dplyr::case_when(
     status_code == "Fed-E" ~ "Endangered",
     status_code == "Fed-T" ~ "Threatened",
     status_code == "Fed-EmE" ~ "Emergency Listing, Endangered",
