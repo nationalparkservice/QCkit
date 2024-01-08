651f345976832ea2e2933573482a718486c274db
Rob Baker <robert_baker@nps.gov>
2024-01-08 10:59:10 -0700

    update lists in function to be more legible

--- R/order_cols.R
+++ R/order_cols.R
@@ -7,6 +7,10 @@
 #'
 #' @details Check to see if you have three (highly) recommended columns (locality, type, basisOfRecord) and various suggested columns present in your dataset. Print a list of which columns are present and which are not. Then, order all the columns in your dataset in the following order: (highly) recommended columns, suggested columns, the rest of the Darwin Core columns, "custom_" (non-Darwin Core) columns, and finally sensitive species data columns.
 #'
+#' Suggested column names include: eventDate, eventDate_flag, scientificName, scientificName_flag, taxonRank, verbatimIdentification, vernacularName, namePublishedIn, recordedBy, individualCount, decimalLongitude, decimalLatitude, coordinate_flag, geodeticDatum", verbatimCoordinates, verbatimCoordinateSystem, verbatimSRS,coordinateUncertaintyInMeters. Note that suggested names include some custom, non-Darwin Core names such as "scientificName_flag".
+#'
+#'
+#'
 #' @param df - This is the dataframe you want to run against the function. To call, simply type df = "the name of your dataframe".
 #'
 #' @return - The function returns a list of required and suggested columns to include in your dataset. When assigning to an object, the object contains your new dataset with all columns ordered properly.
@@ -19,30 +23,55 @@
 #' }
 order_cols <- function(df) {
 
-  suggested <- c("eventDate", "eventDate_flag", "scientificName",
-                 "scientificName_flag", "taxonRank", "verbatimIdentification",
-                 "vernacularName", "namePublishedIn", "recordedBy",
-                 "individualCount", "decimalLongitude", "decimalLatitude",
-                 "coordinate_flag", "geodeticDatum", "verbatimCoordinates",
-                 "verbatimCoordinateSystem", "verbatimSRS",
+  suggested <- c("eventDate",
+                 "eventDate_flag",
+                 "scientificName",
+                 "scientificName_flag",
+                 "taxonRank",
+                 "verbatimIdentification",
+                 "vernacularName",
+                 "namePublishedIn",
+                 "recordedBy",
+                 "individualCount",
+                 "decimalLongitude",
+                 "decimalLatitude",
+                 "coordinate_flag",
+                 "geodeticDatum",
+                 "verbatimCoordinates",
+                 "verbatimCoordinateSystem",
+                 "verbatimSRS",
                  "coordinateUncertaintyInMeters")
 
-  required <- c("locality", "type", "basisOfRecord")
+  recommended <- c("locality", "type", "basisOfRecord")
 
-  allofem <- c("locality", "type", "basisOfRecord", "eventDate",
-               "eventDate_flag", "scientificName", "scientificName_flag",
-               "taxonRank", "verbatimIdentification", "vernacularName",
-               "namePublishedIn", "custom_TaxonomicNotes", "recordedBy",
-               "individualCount", "decimalLongitude", "decimalLatitude",
-               "coordinate_flag", "geodeticDatum", "verbatimCoordinates",
-               "verbatimCoordinateSystem", "verbatimSRS",
+  allofem <- c("locality",
+               "type",
+               "basisOfRecord",
+               "eventDate",
+               "eventDate_flag",
+               "scientificName",
+               "scientificName_flag",
+               "taxonRank",
+               "verbatimIdentification",
+               "vernacularName",
+               "namePublishedIn",
+               "custom_TaxonomicNotes",
+               "recordedBy",
+               "individualCount",
+               "decimalLongitude",
+               "decimalLatitude",
+               "coordinate_flag",
+               "geodeticDatum",
+               "verbatimCoordinates",
+               "verbatimCoordinateSystem",
+               "verbatimSRS",
                "coordinateUncertaintyInMeters")
 
-  print(lapply(required,
+  print(lapply(recommended,
                function(x) ifelse(x %in% names(df),
-                                  paste0("Looking great! The required field \'",
+                                  paste0("Looking great! The recommended field \'",
                                        x, "\' exists within your data"),
-                                  paste0("Please include the required field \'",
+                                  paste0("Please include the recommended field \'",
                                        x, "\' in your dataset"))))
 
   print(lapply(suggested,
