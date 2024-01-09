df497ba8e14018ef888343008f949e56cfdd9fe0
Rob Baker <robert_baker@nps.gov>
2024-01-09 10:18:39 -0700

    add unit tests for validate_coord()

--- tests/testthat/test-geography.R
+++ tests/testthat/test-geography.R
@@ -3,3 +3,19 @@ test_that("get_park_polygon returns an object of class sfc_POLYGON", {
   expect_equal(class(x)[1], "sfc_POLYGON")
 })
 
+test_that("validate_coord properly validates valid coordinate", {
+  x <- validate_coord(unit_code = "ROMO",
+                      lat = 40.32764,
+                      lon = -105.70421)
+  expect_equal(x, TRUE)
+})
+
+test_that("validate_coord properly validates invalid coordinate", {
+  x <- validate_coord(unit_code = "ROMO",
+                      lat = 40.32764,
+                      lon = 105.70421)
+  expect_equal(x, FALSE)
+})
+
+
+
