5407de33395e9a59be27a420fc6ab42e81745ce5
Rob Baker <robert_baker@nps.gov>
2024-01-09 15:50:07 -0700

    add unit test for get_elevation

---
+++ tests/testthat/test-datastore_interactions.R
@@ -0,0 +1,12 @@
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
+
+testhat("create_datastore_script creates a datastore script", {
+  owner <- "nationalparkservice"
+  repo <- "QCkit"
+  path <- here::here()
+  force <- TRUE
+  dev <- TRUE
+
+})
