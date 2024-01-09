779358354662090817350e5115b94e6531cb128c
Rob Baker <robert_baker@nps.gov>
2024-01-09 14:36:14 -0700

    add unit tests for fuzz_location

--- tests/testthat/test-geography.R
+++ tests/testthat/test-geography.R
@@ -1,8 +1,10 @@
+##### get_park_polygon
 test_that("get_park_polygon returns an object of class sfc_POLYGON", {
   x <- get_park_polygon("ROMO")
   expect_equal(class(x)[1], "sfc_POLYGON")
 })
 
+##### validate_coord
 test_that("validate_coord properly validates valid coordinate", {
   x <- validate_coord(unit_code = "ROMO",
                       lat = 40.32764,
@@ -17,6 +19,7 @@ test_that("validate_coord properly validates invalid coordinate", {
   expect_equal(x, FALSE)
 })
 
+#### get_umt_zone
 test_that("get_utm_zone returns correct utm zone", {
   expect_equal(get_utm_zone(-105.70421), 13)
 })
@@ -29,10 +32,77 @@ test_that("get_utm_zone handles longitudes < -180 decimal degrees", {
   expect_equal(get_utm_zone(-181), NULL)
 })
 
+#### convert_long_to_utm
 test_that("convert_long_to_utm returns correct utm zone", {
   expect_equal(suppressWarnings(convert_long_to_utm(-105.70421)), 13)
 })
 
+#### long2UTM
 test_that("long2UTM returns correct utm zone", {
-  expect_equal(suppressWarnings(convert_long_to_utm(-105.70421)), 13)
+  expect_equal(suppressWarnings(long2UTM(-105.70421)), 13)
+})
+
+#### fuzz_location
+test_that("fuzz_location handles non-numeric lat and lon", {
+  x <- fuzz_location(lat = "non-numeric",
+                     lon = "non-numeric",
+                     coord_ref_sys = 4326,
+                     fuzz_level = "Fuzzed - 1km")
+  expect_equal(x, NULL)
+})
+
+test_that("fuzz_location returns well known text", {
+  x <- fuzz_location(lat = 40.32764,
+                     lon = -105.70421,
+                     coord_ref_sys = 4326,
+                     fuzz_level = "Fuzzed - 1km")
+  expect_equal(is.na(wk::wk_problems(wk::new_wk_wkt(x))), TRUE)
+})
+
+test_that("fuzz_location returns well known text with 10km fuzzing", {
+  x <- fuzz_location(lat = 40.32764,
+                     lon = -105.70421,
+                     coord_ref_sys = 4326,
+                     fuzz_level = "Fuzzed - 10km")
+  expect_equal(is.na(wk::wk_problems(wk::new_wk_wkt(x))), TRUE)
+})
+
+test_that("fuzz_location returns well known text with 100m fuzzing", {
+  x <- fuzz_location(lat = 40.32764,
+                     lon = -105.70421,
+                     coord_ref_sys = 4326,
+                     fuzz_level = "Fuzzed - 100m")
+  expect_equal(is.na(wk::wk_problems(wk::new_wk_wkt(x))), TRUE)
+})
+
+test_that("fuzz_location returns well known text datum 4269", {
+  x <- fuzz_location(lat = 40.32764,
+                     lon = -105.70421,
+                     coord_ref_sys = 4269,
+                     fuzz_level = "Fuzzed - 100m")
+  expect_equal(is.na(wk::wk_problems(wk::new_wk_wkt(x))), TRUE)
+})
+
+test_that("fuzz_location returns well known text datum 32616", {
+  x <- fuzz_location(lat = 40.32764,
+                     lon = -105.70421,
+                     coord_ref_sys = 32616,
+                     fuzz_level = "Fuzzed - 100m")
+  expect_equal(is.na(wk::wk_problems(wk::new_wk_wkt(x))), TRUE)
+})
+
+test_that("fuzz_location returns NULL if coord_ref_sys is not decimal degree WGS84 or UTM/GS84", {
+  x <- fuzz_location(lat = 40.32764,
+                     lon = -105.70421,
+                     coord_ref_sys = 1234567789,
+                     fuzz_level = "Fuzzed - 100m")
+  expect_equal(x, NULL)
+})
+
+test_that("fuzz_location returns well known text for souther hemisphere latitudes", {
+  x <- fuzz_location(lat = -40.32764,
+                     lon = -105.70421,
+                     coord_ref_sys = 32616,
+                     fuzz_level = "Fuzzed - 100m")
+  expect_equal(is.na(wk::wk_problems(wk::new_wk_wkt(x))), TRUE)
 })
