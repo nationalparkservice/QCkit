d3d178354323a2deee25f34f1fb7791f7015c8b9
Rob Baker <robert_baker@nps.gov>
2024-01-10 10:29:21 -0700

    Add unit tests for get_elevation; 83% coverage.

--- tests/testthat/test-elevation.R
+++ tests/testthat/test-elevation.R
@@ -1,7 +1,3 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
-
 need_elev <- data.frame(decimalLongitude = c(-105.73012,
                                                -105.73044,
                                                -105.83044),
@@ -17,6 +13,10 @@ wrong_hemisphere <- data.frame(decimalLongitude = c(105.73012,
                                                    -40.26921)
 )
 
+non_numerics <- data.frame(decimalLongitude = c("test"),
+                               decimalLatitude = c("test")
+)
+
 
 test_that("get_elevation returns the correct columns", {
   x <- get_elevation(df = need_elev,
@@ -24,6 +24,18 @@ test_that("get_elevation returns the correct columns", {
                      decimal_long = "decimalLongitude",
                      spatial_ref = "4326",
                      force = TRUE)
+  expect_equal(names(x), c("decimalLongitude",
+                           "decimalLatitude",
+                           "minimumElevationInMeters",
+                          "maximumElevationInMeters"))
+})
+
+test_that("get_elevation returns the correct columns when verbose", {
+  x <- get_elevation(df = need_elev,
+                     decimal_lat = "decimalLatitude",
+                     decimal_long = "decimalLongitude",
+                     spatial_ref = "4326",
+                     force = FALSE)
   expect_equal(names(x), c("decimalLongitude",
                            "decimalLatitude",
                            "minimumElevationInMeters",
@@ -42,5 +54,32 @@ test_that("get_elevation returns correct columns for GPS in wrong hemisphere", {
                            "maximumElevationInMeters"))
 })
 
-gh_req_json <- httr::content(req, "text")
-gh_req_rjson <- jsonlite::fromJSON(gh_req_json)
+test_that("get_elevation returns correct columns for GPS in wrong hemisphere when verbose", {
+  x <- get_elevation(df = wrong_hemisphere,
+                     decimal_lat = "decimalLatitude",
+                     decimal_long = "decimalLongitude",
+                     spatial_ref = "4326",
+                     force = FALSE)
+  expect_equal(names(x), c("decimalLongitude",
+                           "decimalLatitude",
+                           "minimumElevationInMeters",
+                           "maximumElevationInMeters"))
+})
+
+test_that("get_elevation stops when non-numeric data supplied", {
+  x <- get_elevation(df = non_numerics,
+                     decimal_lat = "decimalLatitude",
+                     decimal_long = "decimalLongitude",
+                     spatial_ref = "4326",
+                     force = TRUE)
+  expect_equal(is.null(x), TRUE)
+})
+
+test_that("get_elevation stops when non-numeric data supplied, verbose", {
+  x <- get_elevation(df = non_numerics,
+                     decimal_lat = "decimalLatitude",
+                     decimal_long = "decimalLongitude",
+                     spatial_ref = "4326",
+                     force = FALSE)
+  expect_equal(is.null(x), TRUE)
+})
