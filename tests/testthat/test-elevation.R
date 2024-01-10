e37d3aff162e63a6486cc9b6a49eabbffb2ba5a6
Rob Baker <robert_baker@nps.gov>
2024-01-10 10:08:47 -0700

    add tests for get_elevartion when force = FALSE

--- tests/testthat/test-elevation.R
+++ tests/testthat/test-elevation.R
@@ -9,6 +9,14 @@ need_elev <- data.frame(decimalLongitude = c(-105.73012,
                                                 40.78333,
                                                 40.26921)
                            )
+wrong_hemisphere <- data.frame(decimalLongitude = c(105.73012,
+                                                    105.73044,
+                                                    105.83044),
+                               decimalLatitude = c(-40.56321,
+                                                   -40.78333,
+                                                   -40.26921)
+)
+
 
 test_that("get_elevation returns the correct columns", {
   x <- get_elevation(df = need_elev,
@@ -21,3 +29,18 @@ test_that("get_elevation returns the correct columns", {
                            "minimumElevationInMeters",
                            "maximumElevationInMeters"))
 })
+
+test_that("get_elevation returns correct columns for GPS in wrong hemisphere", {
+  x <- get_elevation(df = wrong_hemisphere,
+                     decimal_lat = "decimalLatitude",
+                     decimal_long = "decimalLongitude",
+                     spatial_ref = "4326",
+                     force = TRUE)
+  expect_equal(names(x), c("decimalLongitude",
+                           "decimalLatitude",
+                           "minimumElevationInMeters",
+                           "maximumElevationInMeters"))
+})
+
+gh_req_json <- httr::content(req, "text")
+gh_req_rjson <- jsonlite::fromJSON(gh_req_json)
