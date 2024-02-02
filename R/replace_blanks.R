ee3bd02af8f5bd2ad9aea10874a3a4bc0e40d078
Rob Baker <robert_baker@nps.gov>
2024-02-01 20:57:16 -0700

    update with lintr styling

--- R/replace_blanks.R
+++ R/replace_blanks.R
@@ -1,18 +1,24 @@
 #' Replaces all blank cells with NA
 #'
-#' @details `replace_blanks()` is particularly useful for exporting data from a database (such as access) and converting it to a data package with metadata.
+#' @details `replace_blanks()` is particularly useful for exporting data from a
+#' database (such as access) and converting it to a data package with metadata.
 #'
-#' `replace_blanks()` will import all .csv files in the current working directory. The files are then written back out to the same directory, overwriting the old .csv files. Any blank cells in the original .csv files will be replaced with NA.
+#' `replace_blanks()` will import all .csv files in the current working
+#' directory. The files are then written back out to the same directory,
+#' overwriting the old .csv files. Any blank cells in the original .csv files
+#' will be replaced with NA.
 #'
-#' One exception is if a .csv contains NO data (i.e. just column names and no data in any of the cells). In this case, the blanks will not be replaced with NA (as the function cannot determine how many NAs to include).
+#' One exception is if a .csv contains NO data (i.e. just column names and no
+#' data in any of the cells). In this case, the blanks will not be replaced with
+#' NA (as the function cannot determine how many NAs to include).
 #' @param directory String. Path to the file(s) to have blanks replaced with NAs. Defaults to the working directory of the project (here::here())
 #' @return list of data frames (invisibly)
 #' @export
 #'
 #' @examples
-#' \dontrun{
+#' \dontrun {
 #' replace_blanks()
-#' }
+#'  }
 replace_blanks <- function(directory = here::here()) {
   #get list of .csv file names
   my_path <- list.files(path = directory, pattern="*.csv",
@@ -26,9 +32,9 @@ replace_blanks <- function(directory = here::here()) {
   #give each dataframe a name basd on the filename where it originated
   names(my_data) <- gsub(".csv", "", my_path)
   #write each dataframe back to .csv
-  for (i in 1:length(seq_along(my_data))) {
+  for (i in 1:seq_along(my_data)) {
     readr::write_csv(my_data[[i]], file = paste0(directory, "/",
-                  names(my_data)[[i]], ".csv"))
+                                                 names(my_data)[[i]], ".csv"))
   }
   return(invisible())
 }
