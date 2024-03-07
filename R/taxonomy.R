ff171f36f1f8a1ba1ae347128c2dfed1f88d981b
Rob Baker <robert_baker@nps.gov>
2024-03-07 08:19:46 -0700

    remove VPN warning from check_te as we no longer use a VPN.

--- R/taxonomy.R
+++ R/taxonomy.R
@@ -72,7 +72,6 @@ check_te <- function(x, species_col, park_code, expansion=FALSE) {
     error = function(e) {
       cat(crayon::red$bold("ERROR: "),
           "Your connection timed out.\n",
-          "Make sure you are logged on to the VPN before running ",
           crayon::green$bold("te_check()"),
           ".", sep="")
       stop()
