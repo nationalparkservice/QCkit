e6c2d4e6568d13041c73e3038f1bab187916e001
iquevedo123 <issacquevedo2@gmail.com>
2024-05-30 14:17:44 -0600

    Updated documentation after running through lintr

--- R/get_columns_from_files.R
+++ R/get_columns_from_files.R
@@ -17,63 +17,63 @@
 #' get_columns_from_files(wd = setwd("./data examples"), common = TRUE)
 #' }
 #'
-get_columns_from_files <- function(wd = getwd(), common = FALSE){
+get_columns_from_files <- function(wd = getwd(), common = FALSE) {
 
-fileList <- list.files(path = wd, pattern = "\\.csv$") # create a list with the names of all csv's
+  fileList <- list.files(path = wd, pattern = "\\.csv$") # create a list with the names of all csv's
 
-dfList <- suppressMessages(lapply(fileList, readr::read_csv)) # create a nested list of all datasets
+  dfList <- suppressMessages(lapply(fileList, readr::read_csv)) # create a nested list of all datasets
 
-names(dfList) <- fileList # name each list within dfList by the name of it's respective file name
+  names(dfList) <- fileList # name each list within dfList by the name of it's respective file name
 
-maxCols <- list() # create an empty list to find the max number of columns from the provided datasets
+  maxCols <- list() # create an empty list to find the max number of columns from the provided datasets
 
-for(i in 1:max(length(dfList))){
-  maxCols[i] <- length(names(dfList[[i]]))
-} # fill in the list with the number of columns from each dataset
+  for(i in 1:max(length(dfList))){
+    maxCols[i] <- length(names(dfList[[i]]))
+  } # fill in the list with the number of columns from each dataset
 
-maxCols <- max(unlist(maxCols)) # find the max column number from the list
+  maxCols <- max(unlist(maxCols)) # find the max column number from the list
 
-allCols <- tibble::tibble(.rows = maxCols) # create an empty dataframe with the maximum number of rows being the maximum number of columns from the datasets provided
+  allCols <- tibble::tibble(.rows = maxCols) # create an empty dataframe with the maximum number of rows being the maximum number of columns from the datasets provided
 
-for(i in 1:max(length(dfList))){
+  for(i in 1:max(length(dfList))){
 
-  columns <- names(dfList[[i]])
+    columns <- names(dfList[[i]])
 
-  length(columns) <- maxCols
+    length(columns) <- maxCols
 
-  tempdf <- tibble::tibble(columns)
-  names(tempdf)[1] <- names(dfList)[i]
+    tempdf <- tibble::tibble(columns)
 
-  allCols <- cbind(allCols, tempdf)
+    names(tempdf)[1] <- names(dfList)[i]
 
-} # add a list of all columns from each provided dataset to the empty dataframe
+    allCols <- cbind(allCols, tempdf)
 
-df <- unlist(allCols)
+  } # add a list of all columns from each provided dataset to the empty dataframe
 
-y <- list()
-z <- list()
+  df <- unlist(allCols)
 
+  y <- list()
+  z <- list()
 
-for(i in 1:length(df)){
-  y[i] <- length(which(df == df[i]))
-} # find how many times a value within the column names is repeated. Generates a list with the number of times each respective column name is repeated across all column names
+  for(i in 1:length(df)){
 
+    y[i] <- length(which(df == df[i]))
 
-for(i in 1:length(y)){
-  ifelse(
-    y[i] > 1,
-    z[i] <- df[i],
-    next)
-} # if a column name appears more than once across all column names, capture that column in a list (z)
-
-z <- Filter(Negate(is.null), z) # get rid of Null values
-commonCols <- z %>% unique() %>% tibble::tibble() # Filter to a unique list of the column names that appear more than once
-colnames(commonCols)[1] <- "Common Columns"
-
-ifelse(common == FALSE,
-       return(allCols),
-       return(commonCols)
-)
+  } # find how many times a value within the column names is repeated. Generates a list with the number of times each respective column name is repeated across all column names
 
 
+  for(i in 1:length(y)){
+    ifelse(y[i] > 1,
+           z[i] <- df[i],
+           next)
+  } # if a column name appears more than once across all column names, capture that column in a list (z)
+
+  z <- Filter(Negate(is.null), z) # get rid of Null values
+  commonCols <- z %>% unique() %>% tibble::tibble() # Filter to a unique list of the column names that appear more than once
+
+  colnames(commonCols)[1] <- "Common Columns"
+
+  ifelse(common == FALSE,
+    return(allCols),
+    return(commonCols)
+  )
 }
