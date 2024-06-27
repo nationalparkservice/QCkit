72a2cb60e5486498dc4d79fb8bd3626fa7630c28
Rob Baker <robert_baker@nps.gov>
2024-06-27 08:25:59 -0600

    update unit tests to test remove_empty_tables instead of removeEmptyTables

--- tests/testthat/test-removeEmptyTables.R
+++ tests/testthat/test-removeEmptyTables.R
@@ -1,15 +1,15 @@
-test_that("removeEmptyTables works", {
+test_that("remove_empty_tables works", {
   my_list <- list(item_a = tibble::tibble(),
                   item_b = mtcars,
                   item_c = iris)
-  expect_warning(removeEmptyTables(my_list))
-  expect_equal(names(suppressWarnings(removeEmptyTables(my_list))), c("item_b", "item_c"))
+  expect_warning(remove_empty_tables(my_list))
+  expect_equal(names(suppressWarnings(remove_empty_tables(my_list))), c("item_b", "item_c"))
 
   my_list$item_d <- tibble::tibble()
-  expect_warning(removeEmptyTables(my_list))
-  expect_equal(names(suppressWarnings(removeEmptyTables(my_list))), c("item_b", "item_c"))
+  expect_warning(remove_empty_tables(my_list))
+  expect_equal(names(suppressWarnings(remove_empty_tables(my_list))), c("item_b", "item_c"))
 
-  tidy_list <- suppressWarnings(removeEmptyTables(my_list))
-  expect_no_warning(removeEmptyTables(tidy_list))
-  expect_equal(removeEmptyTables(tidy_list), tidy_list)
+  tidy_list <- suppressWarnings(remove_empty_tables(my_list))
+  expect_no_warning(remove_empty_tables(tidy_list))
+  expect_equal(remove_empty_tables(tidy_list), tidy_list)
 })
