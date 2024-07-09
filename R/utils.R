a1f14dce3a10041c7fa2a6a382cf66fa5fc012d2
Rob Baker <robert_baker@nps.gov>
2024-07-09 08:38:06 -0600

    add new function, get_user_email

--- R/utils.R
+++ R/utils.R
@@ -40,3 +40,31 @@ globalVariables(c("any_of",
                   "decimalLatitude",
                   "decimalLongitude",
                   "LatLong_CRS"))
+
+
+#' Retrieves an NPS user's email address
+#'
+#' @details The function accesses the system username and then uses a powershell wrapper to access NPS active directory and supply information about the user. That information is then parsed down to the user's email address.
+#'
+#' This function probaby won't work for anyone outside of NPS and likely won't work for anyone who is not using a Windows machine. So build those prerequisites (NPS = FALSE) in when calling it from within other function.
+#'
+#' @return String. The user's email address.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' email <- get_user_email()
+#' }
+get_user_email <- function() {
+  powershell_command <- '([adsisearcher]\\"(samaccountname=$env:USERNAME)\\").findone().properties.proxyaddresses'
+
+  proxy_addresses <- system2("powershell",
+                             args = c("-command",
+                                      powershell_command),
+                             stdout = TRUE)
+
+  email_address <- stringr::str_subset(proxy_addresses, "@") |>
+    stringr::str_remove("SMTP:")
+
+  return(email_address)
+}
