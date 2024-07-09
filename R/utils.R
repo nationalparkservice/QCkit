882e3c61f56f46094241ab6e00887258872c08ac
Rob Baker <robert_baker@nps.gov>
2024-07-09 08:40:37 -0600

    update API from v6 to v7

--- R/utils.R
+++ R/utils.R
@@ -4,13 +4,13 @@
 .pkgglobalenv <- new.env(parent=emptyenv())
 
 #data_store API base URL:
-assign("QC_ds_api", "https://irmaservices.nps.gov/datastore/v6/rest/", envir=.pkgglobalenv)
+assign("QC_ds_api", "https://irmaservices.nps.gov/datastore/v7/rest/", envir=.pkgglobalenv)
 
 #data_store secure API base URL:
-assign("QC_ds_secure_api", "https://irmaservices.nps.gov/datastore-secure/v6/rest/", envir=.pkgglobalenv)
+assign("QC_ds_secure_api", "https://irmaservices.nps.gov/datastore-secure/v7/rest/", envir=.pkgglobalenv)
 
 #data_store dev api (requires secure)
-assign("QC_ds_dev_api", "https://irmadevservices.nps.gov/datastore-secure/v6/rest/", envir = .pkgglobalenv)
+assign("QC_ds_dev_api", "https://irmadevservices.nps.gov/datastore-secure/v7/rest/", envir = .pkgglobalenv)
 
 .QC_ds_api <- function(x){
   get("QC_ds_api", envir = .pkgglobalenv)
