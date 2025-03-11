a8403f96c5552807731f1e672c14d72882b8a73c
mdkompella <mkompella143@gmail.com>
2025-03-11 11:25:30 -0600

    changed print statements to message

--- R/get_park_names.R
+++ R/get_park_names.R
@@ -81,14 +81,14 @@ get_park_names <- function(df,
   if (length(no_unit_names > 0)) {
     no_unit_names <- as.character(unique(no_unit_names))
     unit_codes_str <- paste(no_unit_names, collapse = ", ")
-    print(paste("The following unit codes were not found:", unit_codes_str))
+    message("The following unit codes were not found:", unit_codes_str)
   }
 
   # warning message for park codes that resolved to multiple names
   if (length(many_unit_names > 0)) {
     many_unit_names <- as.character(unique(many_unit_names))
     unit_codes_str_many <- paste(many_unit_names, collapse = ", ")
-    print(paste("The following unit codes resolved to multiple park names:", unit_codes_str_many))
+    message("The following unit codes resolved to multiple park names:", unit_codes_str_many)
   }
 
   return(df2)
